e997571511746f65b41e1f28fe14c0f2b62adbe5
Albat93 <albat93120@gmail.com>
2025-03-25 15:19:18 +0100

    Refactor class properties and methods for HolbertonCourse and Currency; add Pricing class for handling amounts and currencies

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,15 +1,24 @@
-// 3-currency.js
 export default class Currency {
   constructor(code, name) {
     this._code = code;
     this._name = name;
   }
 
-  get code() { return this._code; }
-  set code(value) { this._code = value; }
+  get name() {
+    return this._name;
+  }
 
-  get name() { return this._name; }
-  set name(value) { this._name = value; }
+  set name(name) {
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
