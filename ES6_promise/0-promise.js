0745fec9d64055418ef07f4b9fd936eaddc66027
Albat93 <albat93120@gmail.com>
2025-04-01 11:26:50 +0200

    Add functions to handle API responses using promises and update README

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can simulate a successful or failed API call here
+    resolve("Response from API");
+  });
+}
