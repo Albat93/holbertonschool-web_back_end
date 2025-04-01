ecae459f8386d9b8363ceaa84328595c6e290535
Albat93 <albat93120@gmail.com>
2025-04-01 10:17:12 +0200

    Refactor getListStudentIds function to improve module export

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
-export default function getListStudentIds(students) {
+function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
   return students.map((student) => student.id);
 }
-// The function getListStudentIds takes an array of student objects as input.
-
+export default getListStudentIds;
