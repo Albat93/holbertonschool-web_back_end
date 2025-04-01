1e41e612e8b69eee8b7bb37cbf9e41efb4b20bb7
Albat93 <albat93120@gmail.com>
2025-04-01 10:23:37 +0200

    Refactor getListStudents function to use a local variable and improve export

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,13 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" }
+export default function getListStudents() {
+  const listStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return listStudents.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
 }
-export default getListStudents;
