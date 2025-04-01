032953b588203e8e993e9cffcaaa6d6a115f4ad9
Albat93 <albat93120@gmail.com>
2025-04-01 11:30:14 +0200

    Refactor API response handling to simulate delayed resolution and streamline error handling

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // You can simulate a successful or failed API call here
-    resolve("Response from API");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 1000);
   });
 }
