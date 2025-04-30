68a7364c432f9c753b10fcfd1a0e86cfcc05b84a
Albat93 <albat93120@gmail.com>
2025-04-30 08:35:57 +0200

    Add log_stats module to display statistics about Nginx logs in MongoDB

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Provides some stats about Nginx logs stored in MongoDB."""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Displays statistics about Nginx request logs in MongoDB."""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
