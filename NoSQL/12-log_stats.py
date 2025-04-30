dc7cf04d888427d48bcd06e6c336dffc6862a963
Albat93 <albat93120@gmail.com>
2025-04-30 08:36:49 +0200

    Refactor log_stats script for improved readability and structure

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-"""Provides some stats about Nginx logs stored in MongoDB."""
+"""script that provides some stats about Nginx logs stored in MongoDB"""
+
+
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Displays statistics about Nginx request logs in MongoDB."""
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
+if __name__ == "__main__":
+    """provide stats about nginx logs"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client['logs']
+    collection = db['nginx']
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
+    print(f"{collection.count_documents({})} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    print(collection.count_documents({"method": "GET", "path": "/status"}),
+          "status check")
 
-
-if __name__ == "__main__":
-    log_stats()
+    client.close()
