5bc21948b082d96d30d09d96850948ee3d870176
Albat93 <albat93120@gmail.com>
2025-05-19 11:54:25 +0200

    Refactor stdin handling for improved readability and consistency

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on("readable", () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
 
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
